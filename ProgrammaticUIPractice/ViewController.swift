00d88856ee1d01be3d9c2521f251e41c8bc964b4
SLRAM <stephier2290@gmail.com>
2019-01-24 17:01:24 -0500

    first commit

---
+++ ProgrammaticUIPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProgrammaticUIPractice
+//
+//  Created by Stephanie Ramirez on 1/24/19.
+//  Copyright © 2019 Stephanie Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
